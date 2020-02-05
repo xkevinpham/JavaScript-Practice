2653392e926f6aca259b27070f7ed43c53774088
xkevinpham <57520301+xkevinpham@users.noreply.github.com>
2020-02-05 21:17:39 +0700

    Solved Fib ()
    solved fib functions

--- fibonacci/fib.js
+++ fibonacci/fib.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    if(x < 0){
+        return "Error";
+    };
+    if(x === 0 ){
+        return 0;
+    };
+
+    let a = 0;
+    let b = 1;
+    for (let i = 1; i < x; i++){
+        const j = b;
+        b = a + b;
+        a = j;
+    }
+    return b;
 
 }
 
