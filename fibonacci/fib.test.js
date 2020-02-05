1586815bc94a57f8a0f7dd4b59d66a60c695c78e
xkevinpham <57520301+xkevinpham@users.noreply.github.com>
2020-02-05 21:18:30 +0700

    added more test scenarios to test fib ()
    added more test scenarios to test fib ()

--- fibonacci/fib.test.js
+++ fibonacci/fib.test.js
@@ -4,11 +4,28 @@ describe('fibonacci', function() {
   it('works', function() {
     expect(fibonacci(4)).toEqual(3);
   });
-  xit('works', function() {
+  it('works', function() {
     expect(fibonacci(6)).toEqual(8);
   });
-  xit('works', function() {
+  it('works', function() {
     expect(fibonacci(10)).toEqual(55);
   });
-  
+  it('works', function() {
+    expect(fibonacci(15)).toEqual(610);
+  });
+  it('works', function() {
+    expect(fibonacci(25)).toEqual(75025);
+  });
+  it('doesn\'t accept negatives', function() {
+    expect(fibonacci(-25)).toEqual("OOPS");
+  });
+  it('DOES accept strings', function() {
+    expect(fibonacci("1")).toEqual(1);
+  });
+  it('DOES accept strings', function() {
+    expect(fibonacci("2")).toEqual(1);
+  });
+  it('DOES accept strings', function() {
+    expect(fibonacci("8")).toEqual(21);
+  });
 });
